594526f8a5471a6d4258693766a423dc8708a9c5
Artur <Artur69Karmov@gmail.com>
2019-03-11 01:23:24 +0300

    first cmmit

--- route_inside_polygon.js
+++ route_inside_polygon.js
@@ -18,61 +18,62 @@ function init() {
     // Чтобы корректно осуществлялись геометрические операции
     // над спроецированным многоугольником, его нужно добавить на карту.
     myMap.geoObjects.add(moscowPolygon);
-    var btn = $('#btn');
-    btn.addEventListener('click', function name(params) {
-      console.log('test');
-    });
-    ymaps
-      .route([[59.939095, 30.315868], [59.714951, 30.401340]])
-      .then(function(res) {
-        // Объединим в выборку все сегменты маршрута.
-        var pathsObjects = ymaps.geoQuery(res.getPaths()),
-          edges = [];
+    var btn = document.getElementById('btn');
 
-        // Переберем все сегменты и разобьем их на отрезки.
-        pathsObjects.each(function(path) {
-          var coordinates = path.geometry.getCoordinates();
-          for (var i = 1, l = coordinates.length; i < l; i++) {
-            edges.push({
-              type: "LineString",
-              coordinates: [coordinates[i], coordinates[i - 1]]
-            });
-          }
-        });
+    btn.addEventListener('click', function () {
+      ymaps
+        .route([[59.939095, 30.315868], [59.714951, 30.401340]])
+        .then(function (res) {
+          // Объединим в выборку все сегменты маршрута.
+          var pathsObjects = ymaps.geoQuery(res.getPaths()),
+            edges = [];
 
-        // Создадим новую выборку, содержащую:
-        // - отрезки, описываюшие маршрут;
-        // - начальную и конечную точки;
-        // - промежуточные точки.
-        var routeObjects = ymaps
+          // Переберем все сегменты и разобьем их на отрезки.
+          pathsObjects.each(function (path) {
+            var coordinates = path.geometry.getCoordinates();
+            for (var i = 1, l = coordinates.length; i < l; i++) {
+              edges.push({
+                type: "LineString",
+                coordinates: [coordinates[i], coordinates[i - 1]]
+              });
+            }
+          });
+
+          // Создадим новую выборку, содержащую:
+          // - отрезки, описываюшие маршрут;
+          // - начальную и конечную точки;
+          // - промежуточные точки.
+          var routeObjects = ymaps
             .geoQuery(edges)
             .add(res.getWayPoints())
             .add(res.getViaPoints())
             .setOptions("strokeWidth", 3)
             .addToMap(myMap),
-          // Найдем все объекты, попадающие внутрь КАД.
-          objectsInMoscow = routeObjects.searchInside(moscowPolygon),
-          // Найдем объекты, пересекающие КАД.
-          boundaryObjects = routeObjects.searchIntersect(moscowPolygon);
-        // Раскрасим в разные цвета объекты внутри, снаружи и пересекающие КАД.
-        boundaryObjects.setOptions({
-          strokeColor: "#06ff00",
-          preset: "islands#greenIcon"
-        });
-        objectsInMoscow.setOptions({
-          strokeColor: "#ff0005",
-          preset: "islands#redIcon"
-        });
-        // Объекты за пределами КАД получим исключением полученных выборок из
-        // исходной.
-        routeObjects
-          .remove(objectsInMoscow)
-          .remove(boundaryObjects)
-          .setOptions({
-            strokeColor: "#0010ff",
-            preset: "islands#blueIcon"
+            // Найдем все объекты, попадающие внутрь КАД.
+            objectsInMoscow = routeObjects.searchInside(moscowPolygon),
+            // Найдем объекты, пересекающие КАД.
+            boundaryObjects = routeObjects.searchIntersect(moscowPolygon);
+          // Раскрасим в разные цвета объекты внутри, снаружи и пересекающие КАД.
+          boundaryObjects.setOptions({
+            strokeColor: "#06ff00",
+            preset: "islands#greenIcon"
           });
-      });
+          objectsInMoscow.setOptions({
+            strokeColor: "#ff0005",
+            preset: "islands#redIcon"
+          });
+          // Объекты за пределами КАД получим исключением полученных выборок из
+          // исходной.
+          routeObjects
+            .remove(objectsInMoscow)
+            .remove(boundaryObjects)
+            .setOptions({
+              strokeColor: "#0010ff",
+              preset: "islands#blueIcon"
+            });
+        });
+    }, false);
+    
   }
 
   $.ajax({
