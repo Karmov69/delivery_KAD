e8e8100cf78088ff90d1ed4bcef967dff13e0471
Artur <Artur69Karmov@gmail.com>
2019-03-15 21:07:27 +0300

    first cmmit

--- route_inside_polygon.js
+++ route_inside_polygon.js
@@ -49,20 +49,8 @@ function init() {
                 });
               }
             });
-            var activeRoute = route.getActiveRoute();
-            debugger;
-            if (activeRoute) {
-              // Получим протяженность маршрута.
-              var length = route.getActiveRoute().properties.get("distance"),
-                // Вычислим стоимость доставки.
-                price = calculate(Math.round(length.value / 1000)),
-                // Создадим макет содержимого балуна маршрута.
-                balloonContentLayout = ymaps.templateLayoutFactory.createClass(
-                  '<span>Расстояние: ' + length.text + '.</span><br/>' +
-                  '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + price + ' р.</span>');
-              // Зададим этот макет для содержимого балуна.
-              route.options.set('routeBalloonContentLayout', balloonContentLayout);
-            }
+            console.log(route.getActiveRoute());
+            
             // Создадим новую выборку, содержащую:
             // - отрезки, описываюшие маршрут;
             // - начальную и конечную точки;
