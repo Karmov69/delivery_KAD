452e33d03a58b0b80c0bbcefa5d6deec748c6e05
Artur <Artur69Karmov@gmail.com>
2019-03-17 14:04:14 +0300

    first cmmit

--- route_inside_polygon.js
+++ route_inside_polygon.js
@@ -181,18 +181,7 @@ function init() {
    
 
     myMap.events.add('click', function (e) {
-      if (myPlacemark) {
-        myPlacemark.geometry.setCoordinates(coords);
-      }
-      // Если нет – создаем.
-      else {
-        myPlacemark = createPlacemark(coords);
-        myMap.geoObjects.add(myPlacemark);
-        // Слушаем событие окончания перетаскивания на метке.
-        myPlacemark.events.add('dragend', function () {
-          getAddress(myPlacemark.geometry.getCoordinates());
-        });
-      }
+      
       if (!myMap.balloon.isOpen()) {
         myMap.geoObjects.removeAll();
         myMap.geoObjects.add(moscowPolygon);
@@ -216,9 +205,20 @@ function init() {
               }
             });
             
+            
+            if (myPlacemark) {
+              myPlacemark.geometry.setCoordinates(coords);
+            }
+            // Если нет – создаем.
+            else {
+              myPlacemark = createPlacemark(coords);
+              myMap.geoObjects.add(myPlacemark);
+              // Слушаем событие окончания перетаскивания на метке.
+              myPlacemark.events.add('dragend', function () {
+                getAddress(myPlacemark.geometry.getCoordinates());
+              });
+            }
             getAddress(coords);
-
-
             var isKAD = moscowPolygon.geometry.contains([coords[0].toPrecision(6), coords[1].toPrecision(6)]);
 
             var distance = parseInt(res.getHumanLength()); //Получаем расстояние
